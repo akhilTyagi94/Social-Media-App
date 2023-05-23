f55bb32e5cf4ef70dc04bdd2543ddd4a107708fc
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-05-24 01:17:37 +0530

    sidebar menu functionality

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// Sidebar
+
+const menuItems = document.querySelectorAll(".menu-item");
+
+// Messages
+const messagesNotification = document.querySelector("#messages-notifications");
+
+const messages = document.querySelector(".messages");
+
+// ======= Sidebar ==========
+
+// remove active class from all menu items
+const changeActiveItem = () => {
+  menuItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActiveItem();
+    item.classList.add("active");
+    if (item.id != "notifications") {
+      document.querySelector(".notification-popup").style.display = "none";
+    } else {
+      document.querySelector(".notification-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
+    }
+  });
+});
+
+// ========== Messages ========
+messagesNotification.addEventListener("click", () => {
+  messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+  messagesNotification.querySelector(".notification-count").style.display =
+    "none";
+  setTimeout(() => {
+    messages.style.boxShadow = "none";
+  }, 2000);
+});
