460341b317723e18eb08e6a8c11d2e57e1280e45
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-05-26 01:32:43 +0530

    Background theme customization

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ var root = document.querySelector(":root");
 
 const colorPalette = document.querySelectorAll(".choose-color span");
 
+const Bg1 = document.querySelector(".bg-1");
+const Bg2 = document.querySelector(".bg-2");
+const Bg3 = document.querySelector(".bg-3");
+
 // ======= Sidebar ==========
 
 // remove active class from all menu items
@@ -155,4 +159,43 @@ colorPalette.forEach((color) => {
 
     root.style.setProperty("--primary-color-hue", primaryHue);
   });
+});
+
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG = () => {
+  root.style.setProperty("--light-color-lightness", lightColorLightness);
+  root.style.setProperty("--white-color-lightness", whiteColorLightness);
+  root.style.setProperty("--dark-color-lightness", darkColorLightness);
+};
+
+Bg1.addEventListener("click", () => {
+  Bg1.classList.add("active");
+  Bg2.classList.remove("active");
+  Bg3.classList.remove("active");
+  window.location.reload();
+});
+
+Bg2.addEventListener("click", () => {
+  darkColorLightness = "95%";
+  whiteColorLightness = "20%";
+  lightColorLightness = "15%";
+
+  Bg2.classList.add("active");
+  Bg1.classList.remove("active");
+  Bg3.classList.remove("active");
+  changeBG();
+});
+
+Bg3.addEventListener("click", () => {
+  darkColorLightness = "95%";
+  whiteColorLightness = "10%";
+  lightColorLightness = "0%";
+
+  Bg3.classList.add("active");
+  Bg1.classList.remove("active");
+  Bg2.classList.remove("active");
+  changeBG();
 });
