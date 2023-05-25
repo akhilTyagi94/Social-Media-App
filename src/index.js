a2095678ca9a286ab74e8b1cb44db9cf349ef56b
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-05-26 01:02:58 +0530

    Primary color customization

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ const fontSizes = document.querySelectorAll(".choose-size span");
 
 var root = document.querySelector(":root");
 
+const colorPalette = document.querySelectorAll(".choose-color span");
+
 // ======= Sidebar ==========
 
 // remove active class from all menu items
@@ -121,5 +123,36 @@ fontSizes.forEach((size) => {
 
     // change font size of the root html element
     document.querySelector("html").style.fontSize = fontSize;
+  });
+});
+
+// remove active class from colors
+const changeActiveColorClass = () => {
+  colorPalette.forEach((colorPicker) => {
+    colorPicker.classList.remove("active");
+  });
+};
+
+// change primary colors
+colorPalette.forEach((color) => {
+  color.addEventListener("click", () => {
+    let primary;
+    changeActiveColorClass();
+
+    if (color.classList.contains("color-1")) {
+      primaryHue = 252;
+    } else if (color.classList.contains("color-2")) {
+      primaryHue = 52;
+    } else if (color.classList.contains("color-3")) {
+      primaryHue = 352;
+    } else if (color.classList.contains("color-4")) {
+      primaryHue = 152;
+    } else if (color.classList.contains("color-5")) {
+      primaryHue = 202;
+    }
+
+    color.classList.add("active");
+
+    root.style.setProperty("--primary-color-hue", primaryHue);
   });
 });
